39fcf630be7089a1142a2a5d54c47a8a78e829cb
P.Baiya <pb543@hydra.kent.ac.uk>
2025-07-03 06:49:31 +0100

    updates

--- test/run_4group.sh
+++ test/run_4group.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#source ~/virtualenv/pt1.4_tf2.1/bin/activate
-python main.py --img_size 1024 --group 4 --batch_size 16 --snapshot ./snapshot/ffhq_4group_750k.pt --file img/1.jpg
-deactivate
+
+python main.py --img_size 1024 --group 4 --batch_size 16 --snapshot ./snapshot/ffhq_4group_750k.pt --dir diffusers/examples/dreambooth/synthetic_dataset2/step1_seed_images/
+
